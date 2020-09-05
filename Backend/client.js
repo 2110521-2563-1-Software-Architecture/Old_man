6fac843d5ec7ca456d9c9fdc93883a3b04d0e363
NonthakornK <lak231928@gmail.com>
2020-09-05 18:40:50 +0700

    Client command

--- Backend/client.js
+++ Backend/client.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
-const io = require("socket.io-client");
-const BASE_URL = 'http://localhost:3000';
+const URL = 'http://localhost:3000';
 
-let socket = io.connect(BASE_URL);
+var processName = process.argv.shift();
+var scriptName = process.argv.shift();
+var command = process.argv.shift();
 
 const listBooks = async () => {
-    const res = await axios.get(`${BASE_URL}/books`);
+    const res = await axios.get(`${URL}/books`);
     const books = res.data;
     console.log(books);
     return books;
@@ -13,18 +14,23 @@ const listBooks = async () => {
 
 const insertBook = async (id, title, author) => {
     var book = { id: parseInt(id), title: title, author: author };
-    let res = await axios.post(`${BASE_URL}/insert`,book);
+    let res = await axios.post(`${URL}/insert`,book);
     console.log(res.data)
 }
 
 const getBook = async (id) => {
-    const res = await axios.get(`${BASE_URL}/book/${id}`);
+    const res = await axios.get(`${URL}/book/${id}`);
     const book = res.data;
     console.log(book);
     return book;
 };
 
 const deleteBook = async (id) => {
-    let res = await axios.delete(`${BASE_URL}/delete/${id}`);
+    let res = await axios.delete(`${URL}/delete/${id}`);
     console.log(res.data)
 }
+
+if (command == "list") listAllBooks();
+else if (command == "insert") insertBook(process.argv[0], process.argv[1], process.argv[2]);
+else if (command == "get") getBook(process.argv[0]);
+else if (command == "delete") deleteBook(process.argv[0]);
