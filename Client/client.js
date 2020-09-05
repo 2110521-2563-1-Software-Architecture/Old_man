2c5667382c3be20e74325899809e3b8d422df588
NonthakornK <lak231928@gmail.com>
2020-09-05 16:43:10 +0700

    Client.js

---
+++ Client/client.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const io = require("socket.io-client");
+const BASE_URL = 'http://localhost:3000';
+
+let socket = io.connect(BASE_URL);
+
+const listBooks = async () => {
+    const res = await axios.get(`${BASE_URL}/books`);
+    const books = res.data;
+    console.log(books);
+    return books;
+};
+
+const insertBook = async (id, title, author) => {
+    var book = { id: parseInt(id), title: title, author: author };
+    let res = await axios.post(`${BASE_URL}/insert`,book);
+    console.log(res.data)
+    socket.emit('insert',book)
+}
+
+const getBook = async (id) => {
+    const res = await axios.get(`${BASE_URL}/book/${id}`);
+    const book = res.data;
+    console.log(book);
+    return book;
+};
+
+const deleteBook = async (id) => {
+    let res = await axios.delete(`${BASE_URL}/delete/${id}`);
+    console.log(res.data)
+}
